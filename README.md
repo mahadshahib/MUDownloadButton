87d4b65136b566c3c95b811cdbacef944598b251
Mamad Shahib <41083095+mahadshahib@users.noreply.github.com>
2022-02-16 12:50:15 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MUDownloadButton
+
+A description of this package.
---
+++ Tests/MUDownloadButtonTests/MUDownloadButtonTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MUDownloadButton
+
+final class MUDownloadButtonTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MUDownloadButton().text, "Hello, World!")
+    }
+}
